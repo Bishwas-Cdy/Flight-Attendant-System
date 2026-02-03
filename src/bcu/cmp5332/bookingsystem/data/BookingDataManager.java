a78bd4b14e32093cc6a2af3af677e723934b39cb
Bishwas-Cdy <bishwas.cdyy@gmail.com>
2026-02-03 16:20:28 +0545

    Completed data package

--- src/bcu/cmp5332/bookingsystem/data/BookingDataManager.java
+++ src/bcu/cmp5332/bookingsystem/data/BookingDataManager.java
@@ -1,22 +1,80 @@
 package bcu.cmp5332.bookingsystem.data;
 
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
+import bcu.cmp5332.bookingsystem.model.Booking;
+import bcu.cmp5332.bookingsystem.model.Customer;
+import bcu.cmp5332.bookingsystem.model.Flight;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+import java.util.Scanner;
 
 public class BookingDataManager implements DataManager {
-    
+
     public final String RESOURCE = "./resources/data/bookings.txt";
 
     @Override
     public void loadData(FlightBookingSystem fbs) throws IOException, FlightBookingSystemException {
-        // TODO: implementation here
+
+        try (Scanner sc = new Scanner(new File(RESOURCE))) {
+            int lineIdx = 1;
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+
+                if (line.trim().isEmpty()) {
+                    lineIdx++;
+                    continue;
+                }
+
+                String[] properties = line.split(SEPARATOR, -1);
+
+                try {
+                    int customerId = Integer.parseInt(properties[0]);
+                    int flightId = Integer.parseInt(properties[1]);
+                    LocalDate bookingDate = LocalDate.parse(properties[2]);
+
+                    Customer customer = fbs.getCustomerByID(customerId);
+                    Flight flight = fbs.getFlightByID(flightId);
+
+                    Booking booking = new Booking(customer, flight, bookingDate);
+
+                    customer.addBooking(booking);
+                    flight.addPassenger(customer);
+
+                } catch (NumberFormatException ex) {
+                    throw new FlightBookingSystemException(
+                            "Unable to parse booking ids on line " + lineIdx + "\nError: " + ex);
+                } catch (FlightBookingSystemException ex) {
+                    throw new FlightBookingSystemException(
+                            "Invalid booking data on line " + lineIdx + "\nError: " + ex.getMessage());
+                } catch (IllegalArgumentException ex) {
+                    throw new FlightBookingSystemException(
+                            "Invalid booking data on line " + lineIdx + "\nError: " + ex.getMessage());
+                }
+
+                lineIdx++;
+            }
+        }
     }
 
     @Override
     public void storeData(FlightBookingSystem fbs) throws IOException {
-        // TODO: implementation here
+
+        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
+
+            for (Customer customer : fbs.getCustomers()) {
+                for (Booking booking : customer.getBookings()) {
+                    out.print(customer.getId() + SEPARATOR);
+                    out.print(booking.getFlight().getId() + SEPARATOR);
+                    out.print(booking.getBookingDate() + SEPARATOR);
+                    out.println();
+                }
+            }
+        }
     }
-    
 }
--- src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
+++ src/bcu/cmp5332/bookingsystem/data/CustomerDataManager.java
@@ -1,21 +1,66 @@
 package bcu.cmp5332.bookingsystem.data;
 
 import bcu.cmp5332.bookingsystem.main.FlightBookingSystemException;
+import bcu.cmp5332.bookingsystem.model.Customer;
 import bcu.cmp5332.bookingsystem.model.FlightBookingSystem;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class CustomerDataManager implements DataManager {
 
     private final String RESOURCE = "./resources/data/customers.txt";
-    
+
     @Override
     public void loadData(FlightBookingSystem fbs) throws IOException, FlightBookingSystemException {
-        // TODO: implementation here
+
+        try (Scanner sc = new Scanner(new File(RESOURCE))) {
+            int lineIdx = 1;
+
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+
+                if (line.trim().isEmpty()) {
+                    lineIdx++;
+                    continue;
+                }
+
+                String[] properties = line.split(SEPARATOR, -1);
+
+                try {
+                    int id = Integer.parseInt(properties[0]);
+                    String name = properties[1];
+                    String phone = properties[2];
+
+                    Customer customer = new Customer(id, name, phone);
+                    fbs.addCustomer(customer);
+
+                } catch (NumberFormatException ex) {
+                    throw new FlightBookingSystemException(
+                            "Unable to parse customer id " + properties[0] + " on line " + lineIdx + "\nError: " + ex);
+                } catch (IllegalArgumentException ex) {
+                    throw new FlightBookingSystemException(
+                            "Invalid customer data on line " + lineIdx + "\nError: " + ex.getMessage());
+                }
+
+                lineIdx++;
+            }
+        }
     }
 
     @Override
     public void storeData(FlightBookingSystem fbs) throws IOException {
-        // TODO: implementation here
+
+        try (PrintWriter out = new PrintWriter(new FileWriter(RESOURCE))) {
+            for (Customer customer : fbs.getCustomers()) {
+                out.print(customer.getId() + SEPARATOR);
+                out.print(customer.getName() + SEPARATOR);
+                out.print(customer.getPhone() + SEPARATOR);
+                out.println();
+            }
+        }
     }
 }
